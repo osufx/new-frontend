3e9598c22c1df208af0e0de58c3dff818d410944
Daniel Dimitrov <bgdppeu@gmail.com>
2017-11-04 00:41:42 +0200

    User lookup

--- helpers/checks.py
+++ helpers/checks.py
@@ -1,10 +1,28 @@
+from helpers import mysql
+
 def is_logged_in(request):
 
     username = request.cookies.get('username')
     password = request.cookies.get('password')
 
     if username and password:
+
         return {'username': username}
 
     else:
+
+        return False
+
+def does_user_exist(username):
+
+    connection, cursor = mysql.connect()
+
+    search = mysql.execute(connection, cursor, "SELECT username FROM users WHERE username = %s", [username]).fetchone()
+
+    if search != None and len(search) > 0:
+
+        return True
+
+    else:
+
         return False
