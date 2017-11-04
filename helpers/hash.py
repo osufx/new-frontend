de333ab86f30cff15eeb40ca3331ff95073f3aab
Daniel Dimitrov <bgdppeu@gmail.com>
2017-11-05 00:09:06 +0200

    Added random key generator

--- helpers/hash.py
+++ helpers/hash.py
@@ -1,6 +1,12 @@
 import hashlib
+import random
+import string
 
 def password(p : str):
     sha1 = hashlib.sha1(("i8Ey{}o94C".format(hashlib.md5(("y7Tj{}pLn5".format(p)).encode('utf-8')).hexdigest())).encode('utf-8')).hexdigest()
 
     return sha1
+
+def email_key():
+
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
